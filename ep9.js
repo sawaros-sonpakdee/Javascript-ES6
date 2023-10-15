ead87f867584412802a62397c8aa7188ab60580d
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2023-10-15 17:28:50 +0700

    Create ep9.js

---
+++ ep9.js
@@ -0,0 +1,16 @@
+//function join 
+
+// const data = [100,200,300]
+// console.log(data)
+
+// // const result = data.join()
+// const result = data.join("+")
+// console.log(result)
+
+// function concat การเเอา data มาต่อกัน
+
+const data1 = [100,200,300]
+const data2 = [400,500]
+const alldata = data1.concat(data2)
+
+console.log(alldata)
