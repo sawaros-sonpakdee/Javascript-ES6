30868dd734ba2dc5caa7f00a0b5dd5300d2c2632
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2023-10-15 16:15:19 +0700

    Create ep1.js

---
+++ ep1.js
@@ -0,0 +1,13 @@
+//Block Scope (let/Constant)
+
+//การเขียนแบบเดิม
+
+// let x = 10
+// let y  = 50
+
+// if (x === 10){
+//     let y = 500
+//     console.log("y ใน = "+y);
+// }
+// console.log("y นอก = "+y);
+
